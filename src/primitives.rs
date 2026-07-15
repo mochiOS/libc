8c17d796e4cdbc2be35ad0e1f74ddf2eb14a5c25
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-07-15 17:05:44 +0900

    fix

--- src/primitives.rs
+++
@@ -1,1 +0,0 @@
-/home/jine/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.185/src/primitives.rs
--- src/unix/newlib/mod.rs
+++
@@ -1,3 +0,0 @@
-use core::{concat, env, include};
-
-include!(concat!(env!("OUT_DIR"), "/newlib_mod.rs"));
